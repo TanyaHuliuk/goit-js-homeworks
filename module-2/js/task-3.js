4600125b7200fd95fb7819ae897cdd78802d74d9
Tanya <fullstackon@gmail.com>
2019-11-17 23:31:25 +0200

    homework2

--- module-2/js/task-3.js
+++ module-2/js/task-3.js
@@ -40,3 +40,14 @@ console.log(findLongestWord('May the force be with you'));
 //   console.log(findLongestWord('Google do a roll'));
 
 //   console.log(findLongestWord('May the force be with you'));
+
+// ЗАДАНИЕ 3=======================
+// Напиши фукцнию findLongestWord(string), которая принимает параметром произвольную строку (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
+
+// Вызовы функции для проверки работоспособности твоей реализации.
+
+// console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+// console.log(findLongestWord('Google do a roll')); // 'Google'
+
+// console.log(findLongestWord('May the force be with you')); // 'force'
--- module-2/js/task-5.js
+++ module-2/js/task-5.js
@@ -1,14 +1,12 @@
 'use strict';
 
 const checkForSpam = function(string) {
-  let Str = string.split(/\b/);
-
-  for (let i = 0; i < Str.length; i++) {
-    let word = Str[i].toLowerCase();
-
-    if (word === 'spam' || word === 'sale') {
-      return true;
-    }
+  const Words = ['spam', 'sale'];
+  if (
+    string.toLowerCase().includes('spam') ||
+    string.toLowerCase().includes('sale')
+  ) {
+    return true;
   }
   return false;
 };
@@ -20,3 +18,16 @@ console.log(checkForSpam('JavaScript weekly newsletter')); // false
 console.log(checkForSpam('Get best sale offers now!')); // true
 
 console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
+// Задание 5
+// Напиши функцию checkForSpam(str), принимающую 1 параметр string - строку. Функция проверяет ее на содержание слов spam и sale. Если нашли зарещенное слово то функция возвращает true, если запрещенных слов нет функция возвращает false. Слова в строке могут быть в произвольном регистре.
+
+// Вызовы функции для проверки работоспособности твоей реализации.
+
+// console.log(checkForSpam('Latest technology news')); // false
+
+// console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+// console.log(checkForSpam('Get best sale offers now!')); // true
+
+// console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
