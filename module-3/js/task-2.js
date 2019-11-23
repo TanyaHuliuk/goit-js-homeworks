161ce4bc2fb571a25f223370b8367b48803dc598
Tanya <fullstackon@gmail.com>
2019-11-23 23:28:33 +0200

    homework3

---
+++ module-3/js/task-2.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const countProps = function({ ...args }) {
+  let total = 0;
+  total += Object.keys(args).length;
+  return `${total}`;
+};
+
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: 'Mango', age: 2 })); // 2
+
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
