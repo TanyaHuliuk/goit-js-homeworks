b9d5f8d0e96d23d23f268b72431a21a831d43081
Tanya <fullstackon@gmail.com>
2019-12-24 14:14:22 +0200

    homework4

--- module-4/js/task-2.js
+++ module-4/js/task-2.js
@@ -19,13 +19,13 @@ const invokeInventoryAction = function(itemName, action) {
   action.call(inventory, itemName);
 };
 
-inventory.invokeInventoryAction('Medkit', inventory.add);
+invokeInventoryAction('Medkit', inventory.add.bind(inventory));
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-inventory.invokeInventoryAction('Gas mask', inventory.remove);
+invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
 // Invoking action on Gas mask
 // Removing Gas mask from inventory
 console.log(inventory.items); // ['Knife', 'Medkit']
